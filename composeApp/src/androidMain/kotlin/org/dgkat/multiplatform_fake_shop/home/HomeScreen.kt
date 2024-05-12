750b0e9cbd1408294a42d11e846827a62721beb5
dgkat <dgkatoudis@gmail.com>
2024-05-13 02:48:33 +0300

    rnd: home screen composables

--- composeApp/src/androidMain/kotlin/org/dgkat/multiplatform_fake_shop/home/HomeScreen.kt
+++ composeApp/src/androidMain/kotlin/org/dgkat/multiplatform_fake_shop/home/HomeScreen.kt
@@ -1,18 +1,25 @@
 package org.dgkat.multiplatform_fake_shop.home
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material.Card
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import home.presentation.HomeEvent
+import home.presentation.HomeRowState
 import home.presentation.HomeState
-import productMain.presentation.ProductMainEvent
-import productMain.presentation.ProductMainState
+import home.presentation.models.UiHomeProduct
 
 @Composable
 fun HomeScreen(
@@ -20,20 +27,55 @@ fun HomeScreen(
     onEvent: (HomeEvent) -> Unit
 ) {
     Scaffold { paddingValues ->
-        LazyColumn(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(paddingValues),
-            verticalArrangement = Arrangement.spacedBy(16.dp)
-        ) {
-            item {
-                TitleText(title = state.data ?: "empty")
-            }
-        }
+        HomeProductColumn(paddingValues, state)
     }
 }
 
 @Composable
-fun TitleText(title: String) {
-    Text(text = title)
+private fun HomeProductColumn(
+    paddingValues: PaddingValues,
+    state: HomeState
+) {
+    val lazyColumnState = rememberLazyListState()
+
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(paddingValues),
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+        state = lazyColumnState
+    ) {
+        items(items = state.data.values.toList(), key = { row -> row.type }) { homeRowState ->
+            HomeProductRow(homeRowState)
+        }
+
+    }
+}
+
+@Composable
+fun HomeProductRow(
+    rowState: HomeRowState
+) {
+    val lazyRowState = rememberLazyListState()
+    LazyRow(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        state = lazyRowState
+    ) {
+        items(items = rowState.products, key = { product -> product.id }) { product ->
+            HomeProductCard(product)
+        }
+    }
+
+}
+
+@Composable
+fun HomeProductCard(product: UiHomeProduct) {
+    Card(
+        modifier = Modifier.size(64.dp)
+    ) {
+        Text(text = product.title)
+
+    }
+
 }
